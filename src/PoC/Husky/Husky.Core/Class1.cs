81a7ed6583163f30aa84f63b8599a940e33f640e
Svengeance <steve.vernyi@gmail.com>
2020-10-03 05:19:16 -0400

    Initial projects created; target hello-world application is ready to be distributed

---
+++ src/PoC/Husky/Husky.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Husky.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/PoC/Husky/HuskyApp.Installer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HuskyApp.Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldGenerated.HelloWorld.SayHello();
+        }
+    }
+}
