adccaa494e292fe5b28512efadc428596419a3de
Svengeance <steve.vernyi@gmail.com>
2021-01-02 03:44:47 -0500

    Allows users to specify assembly to resolve resources

--- src/PoC/Husky/Husky.Tasks/InstallationContext.cs
+++ src/PoC/Husky/Husky.Tasks/InstallationContext.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Husky.Tasks
 {
     public class InstallationContext
     {
-        private Dictionary<string, string> _variables { get; } = new();
+        public Assembly InstallationAssembly { get; }
+
+        private readonly Dictionary<string, string> _variables = new();
 
         public IReadOnlyDictionary<string, string> Variables => _variables;
+
+        public InstallationContext(Assembly installationAssembly)
+        {
+            InstallationAssembly = installationAssembly;
+        }
         
         public void SetVariable(string key, object value) => _variables[key] = value.ToString() ?? throw new ArgumentNullException(nameof(value));
     }
