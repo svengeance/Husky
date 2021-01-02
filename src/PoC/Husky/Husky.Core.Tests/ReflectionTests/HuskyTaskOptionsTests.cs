8394a974223c56f70ba7acd8d6fe75f66d1c8b33
Svengeance <steve.vernyi@gmail.com>
2021-01-02 04:05:06 -0500

    Adds tests additional ReflectionTests to make sure ever Task has a validator

---
+++ src/PoC/Husky/Husky.Core.Tests/ReflectionTests/HuskyTaskOptionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using FluentValidation;
+using Husky.Core.Workflow;
+using NUnit.Framework;
+
+namespace Husky.Core.Tests.ReflectionTests
+{
+    public class HuskyTaskOptionsTests
+    {
+        [Test]
+        [Category("ReflectionTest")]
+        public void All_tasks_must_implement_a_private_validator()
+        {
+            foreach (var option in GetHuskyTaskOptions())
+            {
+                var validatorType = typeof(AbstractValidator<>).MakeGenericType(option);
+                var privateValidator = option.GetNestedTypes(BindingFlags.NonPublic)
+                                             .FirstOrDefault(f => f.BaseType == validatorType);
+
+                Assert.NotNull(privateValidator, $"{option} does not implement a Validator; please add a private validator class that extends from AbstractValidator<{option.Name}>");
+            }
+        }
+
+        private static IEnumerable<Type> GetHuskyTaskOptions()
+            => typeof(HuskyTaskConfiguration).Assembly
+                                             .GetExportedTypes()
+                                             .Where(w => !w.IsAbstract && w.IsClass)
+                                             .Where(w => w.BaseType == typeof(HuskyTaskConfiguration));
+    }
+}
